2047961bc3e46b2300e3b6b911575007dfc7fb71
Eliot Rojas <eliotrojashernandez@gmail.com>
2016-10-25 09:19:21 -0700

    Add main models controllers

---
+++ simpleCRM/simpleCRM/Controllers/RolesAdminController.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using simpleCRM.Models;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace simpleCRM.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class RolesAdminController : Controller
+    {
+        public RolesAdminController()
+        {
+            context = new ApplicationDbContext();
+            UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+        }
+
+        public RolesAdminController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            UserManager = userManager;
+            RoleManager = roleManager;
+        }
+
+        public UserManager<ApplicationUser> UserManager { get; private set; }
+        public RoleManager<IdentityRole> RoleManager { get; private set; }
+        public ApplicationDbContext context { get; private set; }
+        //
+        // GET: /Roles/
+        public ActionResult Index()
+        {
+            return View(RoleManager.Roles);
+        }
+
+        //
+        // GET: /Roles/Details/5
+        public async Task<ActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+
+            return View(role);
+        }
+
+        //
+        // GET: /Roles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Roles/Create
+        [HttpPost]
+        public async Task<ActionResult> Create(RoleViewModel roleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole(roleViewModel.Name);
+                var roleresult = await RoleManager.CreateAsync(role);
+                if (!roleresult.Succeeded)
+                {
+                    ModelState.AddModelError("", roleresult.Errors.First().ToString());
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Roles/Edit/Admin
+        public async Task<ActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        //
+        // POST: /Roles/Edit/5
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Name,Id")] IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await RoleManager.UpdateAsync(role);
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", result.Errors.First().ToString());
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Roles/Delete/5
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var role = await RoleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return HttpNotFound();
+            }
+            return View(role);
+        }
+
+        //
+        // POST: /Roles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                var role = await RoleManager.FindByIdAsync(id);
+                var result = await RoleManager.DeleteAsync(role);
+                if (!result.Succeeded)
+                {
+                    ModelState.AddModelError("", result.Errors.First().ToString());
+                    return View();
+                }
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
